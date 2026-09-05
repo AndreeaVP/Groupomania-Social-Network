3d24d8ca56d53a07e3034b35e4fb6192f1c0f7c4
daisy2511 <andreeagatu58@yahoo.com>
2026-09-05 10:16:02 +0100

    Modernise Groupomania Interface

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,24 +3,12 @@
 </template>
 
 <style lang="scss">
+@import '@/styles/redesign';
+
 #app {
-  font-family: Georgia, 'Times New Roman', Times, serif, Helvetica, Arial, sans-serif;
+  font-family: Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: #182230;
 }
 </style>
